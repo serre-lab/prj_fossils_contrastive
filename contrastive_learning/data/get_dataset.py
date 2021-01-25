c1da8d5308f012eb14b1a8bb6cd5e6fd208a9588
piperod <ivan.rodriguez5@upr.edu>
2021-01-25 16:15:22 -0500

    removing symmetric batching for supervised

--- contrastive_learning/data/get_dataset.py
+++ contrastive_learning/data/get_dataset.py
@@ -25,16 +25,15 @@ def get_dataset(dataset='cifar_unsup', batch_size=128, val_split=0.2):
     # base dataset
     if dataset == 'cifar_unsup':
         train, val, test = cifar.get_unsupervised(batch_size=batch_size//2, val_split=val_split)
+        # apply symmetric batch mechanism
+        train = train.map(symmetric_batch)
+        test = test.map(symmetric_batch)
+        val = val.map(symmetric_batch)
     elif dataset == 'cifar_sup':
         train, val, test = cifar.get_supervised(batch_size=batch_size//2, val_split=val_split)
     elif dataset == 'leaves':
         raise NotImplementedError('Leaves dataset is not implemented yet')
     else: 
         raise NotImplementedError('Dataset is not implemented yet')
-    
-    # apply symmetric batch mechanism
-    train = train.map(symmetric_batch)
-    test = test.map(symmetric_batch)
-    val = val.map(symmetric_batch)
 
     return train, val, test
