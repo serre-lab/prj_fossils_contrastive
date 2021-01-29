735eafe190defd6cb6c5e1f8293a5f212741fcdf
piperod <ivan.rodriguez5@upr.edu>
2021-01-29 17:22:55 -0500

    adding dirtypnas for test

--- contrastive_learning/losses.py
+++ contrastive_learning/losses.py
@@ -33,9 +33,11 @@ def get_contrastive_loss(temperature=1.0):
     
     return contrastive_loss
 
-def get_supervised_loss(labels, logits):
-    """Compute mean supervised loss over local batch."""
-    losses = tf.keras.losses.CategoricalCrossentropy(
-      from_logits=True, reduction=tf.keras.losses.Reduction.NONE)(labels,
-                                                                  logits)
-    return tf.reduce_mean(losses)
+def get_supervised_loss():
+    
+    def supervised_loss(labels, logits):
+        """Compute mean supervised loss over local batch."""
+        losses = tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)(labels, logits)
+        return tf.reduce_mean(losses)
+    
+    return supervised_loss
--- contrastive_learning/train_contrastive.py
+++ contrastive_learning/train_contrastive.py
@@ -14,34 +14,34 @@ def train_contrastive(train_dataset, val_dataset, test_dataset,
     @tf.function
     def train_step(batch_x):
         with tf.GradientTape() as tape:
-            _, projection = projector(batch_x, training=True)
+            h_enc, projection = projector(batch_x, training=True)
             loss, logits, labels = loss_fn(projection)
             train_accuracy.update_state(tf.argmax(logits, -1), tf.argmax(labels, -1))
         grads = tape.gradient(loss, projector.trainable_weights)
         optimizer.apply_gradients(zip(grads, projector.trainable_weights))
 
-        return loss
+        return loss,h_enc
         
     @tf.function
     def test_step(batch_x):
-        _, projection = projector(batch_x, training=False)
+        h_enc, projection = projector(batch_x, training=False)
         loss, logits, labels = loss_fn(projection)
         test_accuracy.update_state(tf.argmax(logits,-1),tf.argmax(labels,-1))
 
-        return loss
+        return loss,h_enc
     
     # training loop
     for epoch_i in range(epochs):
         # train iterations
         for batch_x in train_dataset:
-            batch_loss = train_step(batch_x)
+            batch_loss, encoder = train_step(batch_x)
             neptune.log("train_loss", batch_loss.numpy())
             neptune.log("train_accuracy", train_accuracy.result.numpy())
             if verbose:
                 tf.print(f"[Epoch {epoch_i}] [Train] {batch_loss} [Accuracy] {train_accuracy.result()}")
         # test iterations
         for batch_x in test_dataset:
-            batch_loss = test_step(batch_x)
+            batch_loss, encoder = test_step(batch_x)
             neptune.log("train_loss", batch_loss.numpy())
             neptune.log("test_accuracy", test_accuracy.result.numpy())
             if verbose:
@@ -49,7 +49,7 @@ def train_contrastive(train_dataset, val_dataset, test_dataset,
     
     # validation 
     for batch_x in val_dataset:
-        batch_loss = test_step(batch_x)
+        batch_loss,encoder = test_step(batch_x)
         tf.print(f"[Epoch {epoch_i}] [Val] {batch_loss}")
     
 
