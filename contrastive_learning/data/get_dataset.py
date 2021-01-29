8fd9f716f66c86d57919a5313bd5d91fcb3463ad
piperod <ivan.rodriguez5@upr.edu>
2021-01-29 16:34:19 -0500

    preprocessing

--- contrastive_learning/data/get_dataset.py
+++ contrastive_learning/data/get_dataset.py
@@ -13,16 +13,17 @@ def augmentations(x, crop_size=22, brightness=0.2, contrast=0.2, saturation=0.2,
     x = tf.image.random_saturation(x, lower=1.0-saturation, upper=1.0+saturation)
     x = tf.image.random_hue(x, max_delta=hue)
     x = tf.image.resize(x, (224, 224))
-    x = tf.clip_by_value(x, 0, 255)
+    x = tf.clip_by_value(x, 0.0, 255.0)
     x = tf.keras.applications.resnet_v2.preprocess_input(x)
     return x
+
 
 
 def symmetric_batch(batch_x):
     return tf.concat([augmentations(batch_x), augmentations(batch_x)], axis=0)
 
-def resize_images(batch_x,width=224,height=224):
-    return tf.image.resize(batch_x,(width,height))
+def resize_images(batch_x, width=224, height=224):
+    return tf.image.resize(x_batch, width, height)
 
 def get_dataset(dataset='cifar_unsup', batch_size=128, val_split=0.2):
     # base dataset
