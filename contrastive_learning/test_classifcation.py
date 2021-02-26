7933ad33e8ac78e54691866d69c444108b5ffb21
piperod <ivan.rodriguez5@upr.edu>
2021-02-26 17:59:53 -0500

    fixing extant

--- contrastive_learning/test_classifcation.py
+++ contrastive_learning/test_classifcation.py
@@ -13,7 +13,7 @@ nb_classes = NB_CLASSES
 
 decode_example = partial(decode_example, num_classes=nb_classes, target_size=(size,size))
 
-raw_image_dataset = tf.data.TFRecordDataset('/media/data_cifs/projects/prj_fossils/data/processed_data/tf_records_2021_v1/images_*.tfrecords')
+raw_image_dataset = tf.data.TFRecordDataset('/media/data_cifs/projects/prj_fossils/data/processed_data/tf_records_2021_v1/images_0.tfrecords')
 train_ds = raw_image_dataset.map(decode_example, num_parallel_calls=-1).batch(batch_size)
 
 
